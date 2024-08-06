c6c5a2faf2755c6889f77247135ff16e9f44c0ee
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-08-07 00:13:28 +0900

    codecrafters submit [skip ci]

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -6,6 +6,8 @@ object Server {
   def main(args: Array[String]): Unit = {
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
+
     val clientSocket = serverSocket.accept() // wait for client
+    clientSocket.getOutputStream.write("+PONG\r\n".getBytes)
   }
 }
